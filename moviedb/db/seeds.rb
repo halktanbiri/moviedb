164a4c169c3b0c9471632e230b77554a507c5905
y15541001 <cihangurturkwest@gmail.com>
2017-12-09 03:30:34 +0000

    finished

--- moviedb/db/seeds.rb
+++ moviedb/db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
+
+movie1 =Movie.create(name:"Sa",published_date:DateTime.now,description:"Açıkladım") 
+movie2 =Movie.create(name:"As",published_date:DateTime.now,description:"Açıkladım") 
+movie3 =Movie.create(name:"Naber",published_date:DateTime.now,description:"Açıkladım") 
+movie4 =Movie.create(name:"İyi Sen?",published_date:DateTime.now,description:"Açıkladım") 
+movie5 =Movie.create(name:"İyi",published_date:DateTime.now,description:"Açıkladım") 
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
